ace3eea4532e7badb51f31a37a8a12d4d6fea241
stryyk3r <stryyk3r_arkade@gmail.com>
2025-09-16 12:41:37 -0400

    Version 1.0.3

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from core.update_checker import UpdateChecker
 apply_overdue_patch(BackupManager)
 
 # Version information
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 UPDATE_CHECK_URL = "https://api.github.com/repos/stryyk3r/ARKADEManager/releases/latest"
 
 
